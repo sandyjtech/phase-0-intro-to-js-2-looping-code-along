e3850301105fe33fd0608665d35582bc772253cc
Sandra <sandyjtech@gmail.com>
2023-04-15 12:18:20 -0700

    added loop functions'

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let emptyArray = [];
+function eventName() {
+
+}
+function writeCards(names, eventName) {
+for (let i = 0; i < names.length; i++){
+ emptyArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+ debugger;
+}
+return emptyArray;
+}
+
+function countDown(a = 10) {
+ 
+    while (a > 0 -1){
+      console.log(a);
+   a--;
+    }
+  return a;
+ }
+countDown(10);
